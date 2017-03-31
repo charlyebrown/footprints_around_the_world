96ce39524a9b38824ae76abe9d72815ad053203a
charlyebrown <charlyetran@lewisu.edu>
2017-03-31 13:26:49 -0400

    Revert "migrations for db"
    This reverts commit 0bff634894a08a5855155d1437df4b42b1639ce9.

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from flask import Flask
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from models import Company
 
 
 @app.route('/')
@@ -19,7 +14,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
 
 if __name__ == '__main__':
     app.run()
